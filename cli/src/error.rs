4ff6f7d51fd6d01156a7b5905bb8b161513026a3
Nulla <Nullaproject@proton.me>
2026-01-19 17:49:22 +0900

    Add error handling enum for CLI errors

---
+++ cli/src/error.rs
@@ -0,0 +1,46 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+        #[error(transparent)]
+        PolkadotService(#[from] polkadot_service::Error),
+
+        #[error(transparent)]
+        SubstrateCli(#[from] sc_cli::Error),
+
+        #[error(transparent)]
+        SubstrateService(#[from] sc_service::Error),
+
+        #[error(transparent)]
+        SubstrateTracing(#[from] sc_tracing::logging::Error),
+
+        #[cfg(not(feature = "pyroscope"))]
+        #[error("Binary was not compiled with `--feature=pyroscope`")]
+        PyroscopeNotCompiledIn,
+
+        #[cfg(feature = "pyroscope")]
+        #[error("Failed to connect to pyroscope agent")]
+        PyroscopeError(#[from] pyroscope::error::PyroscopeError),
+
+        #[error("Failed to resolve provided URL")]
+        AddressResolutionFailure(#[from] std::io::Error),
+
+        #[error("URL did not resolve to anything")]
+        AddressResolutionMissing,
+
+        #[error("Command is not implemented")]
+        CommandNotImplemented,
+
+        #[error(transparent)]
+        Storage(#[from] sc_storage_monitor::Error),
+
+        #[error("Other: {0}")]
+        Other(String),
+
+        #[error("This subcommand is only available when compiled with `{feature}`")]
+        FeatureNotEnabled { feature: &'static str },
+}
+
+impl From<String> for Error {
+        fn from(s: String) -> Self {
+                Self::Other(s)
+        }
+}
